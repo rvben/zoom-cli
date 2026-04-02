37e70a40fe15bdb3ee3528f8e74f181aa26a8f86
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-04-02 15:03:29 +0200

    chore: use zoomcli as PyPI package name (zoom-cli is blocked as a trademark)

--- zoom_cli/__init__.py
+++ zoom_cli/__init__.py
@@ -4,7 +4,7 @@ zoom-cli: Agent-friendly CLI for the Zoom API.
 
 try:
     from importlib.metadata import version
-    __version__ = version("zoom-cli")
+    __version__ = version("zoomcli")
 except ImportError:
     from importlib_metadata import version
-    __version__ = version("zoom-cli")
+    __version__ = version("zoomcli")
